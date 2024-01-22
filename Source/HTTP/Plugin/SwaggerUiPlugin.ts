364f2cc8243111bce07526cc81ce4730ab702ec0
Ruby <necrelox@proton.me>
2024-01-22 17:07:58 +0100

    feat: add logo, fav ico .. on swagger

--- Source/HTTP/Plugin/SwaggerUiPlugin.ts
+++ Source/HTTP/Plugin/SwaggerUiPlugin.ts
@@ -1,17 +1,69 @@
 import { FastifyInstance } from 'fastify';
-import fastifySwaggerUi from '@fastify/swagger-ui';
+import { fastifySwaggerUi, FastifySwaggerUiOptions } from '@fastify/swagger-ui';
+import * as fs from 'fs';
+import * as path from 'path';
 
 import { IPlugin } from '@/HTTP/Interface';
-import { EnvironmentConfiguration } from '@/Config';
+import { EnvironmentConfiguration, packageJsonConfiguration } from '@/Config';
 
 export class SwaggerUiPlugin implements IPlugin {
     configure(app: FastifyInstance): void {
-        const swaggerUiOptions = {
+        const pathAssets: string = path.join(__dirname, '../Assets');
+        const swaggerUiOptions: FastifySwaggerUiOptions = {
             routePrefix: `${EnvironmentConfiguration.env.PREFIX}/swagger`,
-            exposeRoute: true,
             theme: {
-                title: 'Recup Plast API',
+                title: `${packageJsonConfiguration.name} - API`,
+                favicon: [
+                    {
+                        filename: 'android-chrome-192x192',
+                        rel: 'icon',
+                        type: 'image/png',
+                        sizes: '192x192',
+                        content: fs.readFileSync(path.join(pathAssets, '/Logo/android-chrome-192x192.png')),
+                    },
+
+                    {
+                        filename: 'android-chrome-512x512',
+                        rel: 'icon',
+                        type: 'image/png',
+                        sizes: '512x512',
+                        content: fs.readFileSync(path.join(pathAssets, '/Logo/android-chrome-512x512.png')),
+                    },
+                    {
+                        filename: 'apple-touch-icon',
+                        rel: 'icon',
+                        type: 'image/png',
+                        sizes: '180x180',
+                        content: fs.readFileSync(path.join(pathAssets, '/Logo/apple-touch-icon.png')),
+                    },
+                    {
+                        filename: 'favicon-16x16',
+                        rel: 'icon',
+                        type: 'image/png',
+                        sizes: '16x16',
+                        content: fs.readFileSync(path.join(pathAssets, '/Logo/favicon-16x16.png')),
+                    },
+                    {
+                        filename: 'favicon-32x32',
+                        rel: 'icon',
+                        type: 'image/png',
+                        sizes: '32x32',
+                        content: fs.readFileSync(path.join(pathAssets, '/Logo/favicon-32x32.png')),
+                    },
+                    {
+                        filename: 'favicon',
+                        rel: 'shortcut icon',
+                        type: 'image/x-icon',
+                        sizes: '16x16',
+                        content: fs.readFileSync(path.join(pathAssets, '/Logo/favicon.ico')),
+                    },
+                ] as { filename: string; rel: string; type: string; sizes: string; content: string | Buffer; }[]
+            },
+            logo: {
+                type: 'image/png',
+                content: fs.readFileSync(path.join(pathAssets, '/Logo/logo.png')),
             }
+
         };
         app.register(fastifySwaggerUi, swaggerUiOptions);
     }
